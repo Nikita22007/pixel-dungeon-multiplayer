26298de3b104f8ccffdedcc72a0c45551ef26ccb
Nikita22007 <game.nikita22007@yandex.ru>
2023-01-08 13:41:57 +0300

    bags icon

--- core/src/main/java/com/watabou/pixeldungeon/items/bags/CustomBag.kt
+++ core/src/main/java/com/watabou/pixeldungeon/items/bags/CustomBag.kt
@@ -5,27 +5,36 @@ import com.watabou.pixeldungeon.actors.Char
 import com.watabou.pixeldungeon.items.CustomItem
 import com.watabou.pixeldungeon.items.Item
 import com.watabou.pixeldungeon.ui.Icons
+import com.watabou.pixeldungeon.utils.GLog
 import org.json.JSONArray
 import org.json.JSONObject
 
 class CustomBag(obj: JSONObject) : Bag(obj) {
-    public var icon: Icons;
+    public var icon: Icons = Icons.BACKPACK;
+
     init {
-        icon = Icons.BACKPACK;
         cursedKnown = true // todo check it
         size = obj.getInt("size");
-        if (obj.has("owner")){
+        if (obj.has("owner")) {
             owner = Actor.findById(obj.getInt("owner")) as Char?
         }
         if (obj.has("items")) {
             addItemsFromJSONArray(obj.getJSONArray("items"))
+        }
+        if (obj.has("icon")) {
+            try {
+                Icons.valueOf(obj.getString("items"))
+            } catch (e: RuntimeException) {
+                GLog.n("incorrect icon: " + e.message);
+                e.printStackTrace();
+            }
         }
     }
 
     private fun addItemsFromJSONArray(arr: JSONArray) {
         for (i in 0 until arr.length()) {
             val itemObj = arr.getJSONObject(i);
-            val item: Item = CustomItem.createItem(itemObj)
+            val item: Item = createItem(itemObj)
             items.add(item)
         }
     }
